6452fedee1ab0e28b8008c7f9ec12ffb25e6275e
Jashmandeep-Kaur-Bal <24BAI70086@cuchd.in>
2026-08-13 16:19:29 +0530

    Time: 63 ms (5.11%) | Memory: 84.9 MB (34.64%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> count;
+        int low=0;
+        int ans=0;
+        for(int high=0;high<fruits.size();high++){
+            count[fruits[high]]++;
+            while(count.size()>2){
+                count[fruits[low]]--;
+                if(count[fruits[low]]==0){
+                    count.erase(fruits[low]);
+                }
+                low++;
+            }
+            ans=max(ans, high-low+1);
+        }
+        return ans;
+    }
+};
